c79ca12fa7fb709f525451dda132808bef72bc9c
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-03-25 12:11:59 +0530

    main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from datetime import datetime
 import smtplib
-import time
 from tkinter import *
+import random
 
 my_email = "agarwaltushar3710@gmail.com"
 my_pass = "ohhn vfpe ozih yccf"
@@ -22,6 +22,13 @@ def get_quote():
     #print(author)
     canvas.itemconfig(quote_text, text=complete)
 
+#choosing fonts
+with open("fonts.txt") as file:
+    all_fonts = file.readlines()
+    choosed_font = random.choice(all_fonts)
+
+print(choosed_font)
+
 #choosing image templte from images dir
 window = Tk()
 window.title("Monday be like...")
@@ -30,8 +37,9 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="images.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="motivation", width=250, font=("Arial", 30, "bold"), fill="black")
+quote_text = canvas.create_text(150, 207, text="motivation", width=250, font=(choosed_font, 30), fill="black")
 canvas.grid(row=0, column=0)
+
 
 next_quote = "Next Plz"
 button = Button(text =next_quote, highlightthickness=0, command=get_quote)
