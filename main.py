8fa0f0704d7b921bd4baf72338bfb7568a912f9c
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-03-25 11:44:05 +0530

    main

--- main.py
+++ main.py
@@ -22,3 +22,19 @@ def get_quote():
     #print(author)
     canvas.itemconfig(quote_text, text=complete)
 
+#choosing image templte from images dir
+window = Tk()
+window.title("Monday be like...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="images.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="motivation", width=250, font=("Arial", 30, "bold"), fill="black")
+canvas.grid(row=0, column=0)
+
+next_quote = "Next Plz"
+button = Button(text =next_quote, highlightthickness=0, command=get_quote)
+button.grid(row=1, column=0)
+
+window.mainloop()
