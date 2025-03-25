2f244cda20a843a908267ddbbf3e36327156010f
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-03-25 11:43:54 +0530

    main

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ my_pass = "ohhn vfpe ozih yccf"
 def mail():
     pass
 
+#fetching quotes with API
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/quotes/random")
+    response.raise_for_status()
+    data = response.json()
+    quote = data[1]["q"]
+    author = data[1]["a"]
+    complete = f"{quote}\n\n~{author}"
+    #print(quote)
+    #print(author)
+    canvas.itemconfig(quote_text, text=complete)
+
