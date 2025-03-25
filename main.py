129c1046f3d92063de4da281ea4f199a70e3eba1
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-03-25 11:43:28 +0530

    main

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+from tkinter import *
+
+my_email = "agarwaltushar3710@gmail.com"
+my_pass = "ohhn vfpe ozih yccf"
+
+def mail():
+    pass
+
